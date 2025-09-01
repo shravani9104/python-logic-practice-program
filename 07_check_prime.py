312e178d8178e70c89efe9f0b54d2a7b5506e29d
Shravani Nilesh Patil <shravani9104@gmail.com>
2025-09-01 22:30:26 +0530

    Added program to check prime  numbers

---
+++ 07_check_prime.py
@@ -0,0 +1,17 @@
+num = int(input("enter a number : "))
+
+# 1 and below are not prime
+if num <= 1 :
+    print(num, "is not a prime number.")
+else:
+    is_prime = True #assume prime first 
+
+# check divisibility from 2 to square root of num
+    for i in range(2, int(num**0.5) + 1):
+        if num % i == 0: #if divisible by i
+            is_prime = False
+            break
+    if is_prime:
+        print(num, "is a prime number")
+    else:
+        print(num,"is not a prime number")
